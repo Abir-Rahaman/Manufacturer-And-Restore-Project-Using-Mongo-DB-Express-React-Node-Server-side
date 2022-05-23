c4436a8e51a945a06f1d113dcec04a120090fc02
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-05-23 21:03:10 +0600

    parts & tools data load done

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 4000;
+require ('dotenv').config();
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.NAME}:${process.env.PASS}@computer-shop.oyvkd.mongodb.net/?retryWrites=true&w=majority";`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const computerCollection = client.db("Computer_Shop").collection("Parts");
+
+        // get all tools [http://localhost:4000/computer]
+        app.get('/computer', async(req,res)=>{
+            const query = {}
+            const cursor = computerCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+       
+    }
+    finally{
+        // client.close();
+
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello from Computer shop')
+  })
+  
+ app.listen(port, () => {
+    console.log(` Done With Computer shop ${port}`)
+  })
+run().catch(console.dir);

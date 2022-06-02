0c6a740b422cd8aca761f9e63fbed9d9efa675d6
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-06-02 22:10:14 +0600

    alhmdulillah  problem solved

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
 const port = process.env.PORT || 4000;
 
 
-app.use(cors({origin:'https://assignment-12-authentication.web.app/'}))
+app.use(cors({origin:'https://assignment-12-authentication.web.app'}))
 // app.use(cors());
 app.use(express.json())
 
